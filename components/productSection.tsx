cb6d87328349eb29a5f1c10446d0f5b475b7faf1
Juli2kapo <sestojulian@gmail.com>
2025-09-03 15:50:12 -0300

    removed demos correctly

--- components/productSection.tsx
+++ components/productSection.tsx
@@ -321,7 +321,7 @@ export default function ProductSection() {
 
         </div>
 
-        <div className="mb-5 mx-[5vw]">
+        {/* <div className="mb-5 mx-[5vw]">
           <div className="bg-gradient-to-r  border border-purple-500/20 rounded-2xl p-8 text-center backdrop-blur-sm mb-12">
             <h3 className="text-white text-[3.5vh] font-bold mb-4">
               {demoTitle}
@@ -330,7 +330,7 @@ export default function ProductSection() {
               {demoSubtitle}
             </p>
 
-            {/* <div className="flex flex-col items-center space-y-4">
+            <div className="flex flex-col items-center space-y-4">
               <div className="flex items-center justify-center space-x-3 bg-black/40 rounded-full px-6 py-4 border border-purple-400/50">
                 <Phone size={24} color="#a855f7" className="animate-pulse" />
                 <span className="text-white text-[2.5vh] font-semibold tracking-wider">
@@ -344,9 +344,9 @@ export default function ProductSection() {
                   "Llama ahora para experimentar nuestro asistente de IA en acción"
                 }
               </p>
-            </div> */}
+            </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   )
